aab7279e1c27b3202fe7c3a75c517448ff40aba8
Chemseddine MENAKBI <m.chemseddine@gmail.com>
2020-12-27 21:25:38 +0100

    first commit

---
+++ intro-to-mongodb/mflix/mflix 2/env.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=mflix/mflix.py
+set FLASK_DEBUG=false
+set MFLIX_DB_URI="mongodb+srv://analytics:analytics-password@mflix.fln61.mongodb.net/mflix" & REM Replace XXXX with your MongoDB Connection URI
---
+++ intro-to-mongodb/mflix/mflix 2/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export FLASK_APP=mflix/mflix.py
+export FLASK_DEBUG=false
+export MFLIX_DB_URI="mongodb+srv://analytics:analytics-password@mflix.fln61.mongodb.net/mflix" # Replace XXXX with your MongoDB Connection URI
